8af1a1fbb80f596833c581f062f18652c72cbe2a
Dhanush G Shetty <dhanushgshetty666@gmail.com>
2026-06-16 10:19:34 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned=""
+        for chr in s:
+            if chr.isalnum():
+                cleaned+=chr.lower()
+        return cleaned==cleaned[::-1]
+    
